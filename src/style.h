2003710961a610c16db4d5ae35a06b1a45cf58fd
Bornageek <info@bornageek.com>
2015-08-28 14:10:45 +0200

    Basic implementation of styles.

---
+++ src/style.h
@@ -0,0 +1,44 @@
+#include <string>
+
+namespace bornageek {
+  namespace utils {
+    namespace termtab {
+      enum Alignment {
+        LEFT, RIGHT, CENTER
+      };
+
+      class Style {
+        private:
+          std::string mBorderX = "-";
+          std::string mBorderY = "|";
+          std::string mBorderE = "+";
+
+          std::uint16_t mPaddingLeft = 1;
+          std::uint16_t mPaddingRight = 1;
+
+          std::uint16_t mWidth = 0;
+
+        public:
+          Style();
+
+          const std::string& borderX() const;
+          void borderX(std::string const &borderStyle);
+
+          const std::string& borderY() const;
+          void borderY(std::string const &borderStyle);
+
+          const std::string& borderE() const;
+          void borderE(std::string const &borderStyle);
+
+          const std::uint16_t paddingLeft() const;
+          void paddingLeft(std::uint16_t const &padding);
+
+          const std::uint16_t paddingRight() const;
+          void paddingRight(std::uint16_t const &padding);
+
+          const std::uint16_t width() const;
+          void width(std::uint16_t const &width);
+      };
+    }
+  }
+}
